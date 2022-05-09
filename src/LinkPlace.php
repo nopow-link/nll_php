cb998777f11ab7ce7711d90d914085716575afc3
CallMarl <PaHaGeek@gmail.com>
2022-05-09 17:51:10 +0200

    FEAT: Ajout d'un parser HTML

---
+++ src/LinkPlace.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace NllLib;
+
+class LinkPlace
+{
+
+	private $links;
+
+	public function __construct($links)
+	{
+		$this->links = $links
+	}
+
+	public function get_links()
+	{
+		return $this->links
+	}
+
+	public function place(html)
+	{
+		
+	}
+}
