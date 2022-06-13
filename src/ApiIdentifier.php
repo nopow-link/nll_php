e9fca8990923a4a4086421b5ace6783ca26e32bf
CallMarl <PaHaGeek@gmail.com>
2022-06-13 14:59:15 +0200

    FIX: Url de requête d'API, pour éviter les redirections

--- src/ApiIdentifier.php
+++ src/ApiIdentifier.php
@@ -6,13 +6,13 @@ class ApiIdentifier
 {
 	public static function certify($api_key)
 	{
-		return sprintf("api/public/plugin/%s/certify", $api_key);
+		return sprintf("api/public/plugin/%s/certify/", $api_key);
 	}
 
 	public static function collect($api_key, $slug)
 	{
 		return sprintf(
-			"api/public/plugin/%s/collect?slug=%s",
+			"api/public/plugin/%s/collect/?slug=%s",
 			$api_key,
 			$slug
 			);
